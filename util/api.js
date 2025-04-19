03dafecc33a87093193c80b41cf9e8690210e947
Rosgen Hizer <rokhai.dev@gmail.com>
2025-04-19 22:27:38 +0800

    update: api to vercel uri update: gitignore

--- util/api.js
+++ util/api.js
@@ -1,7 +1,12 @@
+// require('dotenv').config();
 import axios from 'axios';
 
+const prod_url = "https://recipe-book-backend-six.vercel.app/"
+// console.log('API Base URL:', process.env.REACT_APP_API_BASEURL);
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // Backend URL
+    baseURL: prod_url, // Backend URL
+    // baseURL: process.env.REACT_APP_API_BASEURL, // Backend URL
     withCredentials: true, // Include cookies in requests
 });
 
