9e924aa09e8ee0a2757f47ee8d51ff1d26473d13
Rosgen Hizer <rokhai.dev@gmail.com>
2025-04-19 19:03:38 +0800

    add: .env file update: app to redirect automatically in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Login from '../pages/Login/Login'
+import { useNavigate } from 'react-router'
+import React, { useEffect } from 'react'
 
 function App() {
+  const navigate = useNavigate();
 
-  return (
-      <Login />
-  )
+  useEffect(() => {
+    navigate('/login'); // Navigate to the login page when the component mounts
+  }, [navigate]);
+
+  return null; // No UI is rendered in this component
 }
 
 export default App
